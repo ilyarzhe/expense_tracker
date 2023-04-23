4e430be7ae6f384357a7c0bccc17f5df4bebf3d7
Ilya Rzheznikov <ilyarzhe@gmail.com>
2023-04-23 08:58:08 +0100

    fix category controller

--- src/main/java/com/example/expensetracker/controllers/CategoryController.java
+++ src/main/java/com/example/expensetracker/controllers/CategoryController.java
@@ -4,10 +4,10 @@ import com.example.expensetracker.models.Category;
 import com.example.expensetracker.models.Expense;
 import com.example.expensetracker.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +20,23 @@ public class CategoryController {
     @GetMapping
     public ResponseEntity<List<Category>> findAllCategories(){
         return ResponseEntity.ok(categoryService.findAllCategories());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> findCategoryById(@PathVariable Long id){
+        return categoryService.findCategoryById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category){
+        return ResponseEntity.status(HttpStatus.CREATED).body(category);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id){
+        return ResponseEntity.noContent().build();
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category category){
+        category.setId(id);
+        return ResponseEntity.ok(categoryService.saveCategory(category));
+
     }
 }
